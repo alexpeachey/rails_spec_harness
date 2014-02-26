e15abce444d5127985b797b121fd5ee4e6fe5001
alexpeachey <alex.peachey@gmail.com>
2014-02-25 16:25:53 -0800

    Wire up the gemspec

--- lib/rails_spec_harness.rb
+++ lib/rails_spec_harness.rb
@@ -1,5 +1,8 @@
-require "rails_spec_harness/version"
+require 'rails_spec_harness/version'
+require 'generators/harness_gems/harness_gems_generator'
+require 'generators/harness_guardfile/harness_guardfile_generator'
+require 'generators/harness_spec_helper/harness_spec_helper_generator'
+require 'generators/spec_harness/spec_harness_generator'
 
 module RailsSpecHarness
-  # Your code goes here...
 end
