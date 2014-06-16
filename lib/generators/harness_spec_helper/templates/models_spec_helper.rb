9ffe4cf07924aac3d610ad9e0e12cf5f80587618
Nick Shook <nicholas.shook@gmail.com>
2014-06-15 21:18:37 -0700

    Changed active_record file for RSpec 3.
    This let me run the models_specs fast, though I do not know if/when you
    want to support RSpec3.

--- lib/generators/harness_spec_helper/templates/models_spec_helper.rb
+++ lib/generators/harness_spec_helper/templates/models_spec_helper.rb
@@ -1,6 +1,6 @@
 require 'base_spec_helper'
 require 'active_record'
-require 'rspec/rails/extensions/active_record/base'
+require 'rspec/rails/extensions/active_record/proxy'
 require 'shoulda'
 Dir[File.join(RAILS_ROOT, "spec/support_models/**/*.rb")].each {|f| require f}
 
